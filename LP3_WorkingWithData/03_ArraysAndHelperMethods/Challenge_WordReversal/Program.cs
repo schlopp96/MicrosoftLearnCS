574b346959cf36c855ece255b5b9298422b1ee63
schlopp96 <71921821+schlopp96@users.noreply.github.com>
2021-12-06 22:55:19 -0600

    Completed LP3/Ch3/Word Reversal Challenge
    Signed-off-by: schlopp96 <71921821+schlopp96@users.noreply.github.com>

--- LP3_WorkingWithData/03_ArraysAndHelperMethods/Challenge_WordReversal/Program.cs
+++ LP3_WorkingWithData/03_ArraysAndHelperMethods/Challenge_WordReversal/Program.cs
@@ -1,20 +1,45 @@
 ﻿/*
         % Word Reversal Challenge %
+            @ 12/4/21 @
 
-< Write the code necessary to reverse the letters of each word in place and display the result.
-< In other words, don't just reverse every letter in the variable pangram.
-< Instead, you'll need to:
-    > reverse just the letters in each word
-    > print the reversed word in its original position in the message.
+$ Write the code necessary to:
+    > reverse the letters of each word in place and display the result.
+    @ In other words, don't just reverse every letter in the variable pangram.
+
+    < Instead, you'll need to:
+        ~ reverse just the LETTERS in each word
+        ~ BUT print the reversed word in its ORIGINAL POSITION in the message.
 
 ! START WITH THIS CODE:
     ? string pangram = "The quick brown fox jumps over the lazy dog";
 
-^ If you're successful, you should see the following output:
-    ~ "ehT kciuq nworb xof spmuj revo eht yzal god"
+$ FINISH WITH THIS CODE:
+    > "ehT kciuq nworb xof spmuj revo eht yzal god"
 
-
+^    ! This is a particularly difficult challenge! !
+> You'll need to combine many of the concepts you've learned in this exercise...
+% Including the use of:
+    & string.Split()
+    & char[] array = value.ToCharArray()
+    & Array.Reverse()
+    & String.Join()
 
 */
+using System;
+
 string pangram = "The quick brown fox jumps over the lazy dog";
-Console.WriteLine($"\nOriginal Pangram: {pangram}\n");
+
+string[] words = pangram.Split(' '); //% Breaks down pangram to new array containing all 9 words.
+string[] pangram_rev = new string[pangram.Length]; //> Creates empty array with exact number of string placeholders as words in pangram for final output.
+//Console.WriteLine($"string pangram = {pangram};\nstring[] words = pangram.Split(' ');");
+//System.Console.WriteLine(string.Concat(words));
+//System.Console.WriteLine($"{string.Concat(words.Length)} total elements in the array.");
+for (int i = 0; i < words.Length; i++)
+{
+    char[] letters = words[i].ToCharArray(); //< break down each word to its individual letters, and adds resulting chars to array.
+    Array.Reverse(letters); // ? REVERSE array of chars from original word 1 by 1
+    pangram_rev[i] = new string(letters); //@ Convert each newly reversed char-array to single element in new string array, and repeat for all words.
+}
+
+string result = String.Join(" ", pangram_rev); //< Rejoin all words together by a space: the same character they were split by.
+Console.WriteLine(result);
