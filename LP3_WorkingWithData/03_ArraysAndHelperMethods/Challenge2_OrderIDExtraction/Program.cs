01bd7a0ef5d32cca0055284f311b12b96f2f2c9f
schlopp96 <71921821+schlopp96@users.noreply.github.com>
2021-12-17 19:32:52 -0600

    - Finished second module challenge. - Finished Chapter 3 in LP3.
    Signed-off-by: schlopp96 <71921821+schlopp96@users.noreply.github.com>

---
+++ LP3_WorkingWithData/03_ArraysAndHelperMethods/Challenge2_OrderIDExtraction/Program.cs
@@ -0,0 +1,32 @@
+﻿/*
+                                $ Challenge - Order ID Extraction
+
+&- Previously, we set out to write code that would store Order IDs belonging to potentially fraudulent orders.
+&- We hope to find those fraudulent orders as early as possible and flag them for deeper analysis.
+
+< - Our team found a pattern -- orders that start with the letter "B" encounter fraud 25 times the normal rate.
+    $ - Our job is to write new code that will output the Order ID of new orders where the Order ID starts with the letter "B".
+    $ - This will be used by our fraud team to investigate further.
+
+> - Using the code below as a starting point, you'll need to parse Order IDs out of a string containing a sequence of incoming orders (the orderStream).
+> - Then, you'll print each Order ID that starts with the letter "B".
+        % Start with this code:
+            ^ string orderStream = "B123,C234,A345,C15,B177,G3003,C235,B179";
+            ^ // Your code here
+        % Output Needed:
+            ^ B123
+            ^ B177
+            ^ B179
+
+*/
+
+string orderStream = "B123,C234,A345,C15,B177,G3003,C235,B179";
+string[] orders = orderStream.Split(',');
+
+foreach (string order in orders)
+{
+    if (order.StartsWith('B'))
+    {
+        Console.WriteLine(order);
+    }
+}
