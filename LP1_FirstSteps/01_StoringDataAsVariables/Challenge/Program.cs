260e9acc95e36335211106c70a12575c523cfa90
schlopp96 <71921821+schlopp96@users.noreply.github.com>
2022-01-11 10:33:24 -0600

    - Small changes to LP1-Challenge
    Signed-off-by: schlopp96 <71921821+schlopp96@users.noreply.github.com>

--- LP1_FirstSteps/01_StoringDataAsVariables/Challenge/Program.cs
+++ LP1_FirstSteps/01_StoringDataAsVariables/Challenge/Program.cs
@@ -12,12 +12,13 @@ namespace Challenge
     {
         static void Main()
         {
-            string name = "Bob";
+            Console.Write("Welcome!\nPlease Enter Your Name:\n> ");
+            string name = Console.ReadLine();
             int messages = 3;
             decimal temperature = 34.4m;
             Console.WriteLine($"Hello, {name}! You have {messages} messages in your inbox.\nThe temperature is {temperature} celsius.");
             Console.WriteLine("Press anything to continue...");
-            Console.ReadKey();
+            Console.ReadKey(false);
         }
     }
 }
